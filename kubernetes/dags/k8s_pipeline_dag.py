06c1d75bc7ed32586352caa5486189e0f7fd2a4d
Guruvendra47 <Guruvendra47@gmail.com>
2026-04-01 15:41:13 -0400

    Update k8s_pipeline_dag.py

--- kubernetes/dags/k8s_pipeline_dag.py
+++ kubernetes/dags/k8s_pipeline_dag.py
@@ -19,7 +19,7 @@ with DAG(
         task_id="spark_job",
         name="spark-job",
         namespace="rtf-data-pipeline",
-        image="spark-job:4.0",
+        image="spark-job:5.0",
         image_pull_policy="Never",
         cmds=["/opt/spark/bin/spark-submit"],
         arguments=["/opt/spark-app/spark-streaming-s3-aws.py"],
